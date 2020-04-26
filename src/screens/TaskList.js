c3b164e0dce8c46d6b4f9c4209c1ce01c6f8f7cf
salubcosta <salubcosta@gmail.com>
2020-04-26 00:29:52 -0300

    updates

---
+++ src/screens/TaskList.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+
+import commonStyles from '../commomStyles';
+import todayImage from '../../assets/imgs/today.jpg';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+export default class TaskList extends Component{
+    render(){
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+        return(
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subTitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
+                
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background:{
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 50,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subTitle:{
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 30
+    }
+});
